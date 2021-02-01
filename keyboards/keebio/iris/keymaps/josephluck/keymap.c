0ae17d75943f59fe712867a89817b32b1a87d694
Joseph Luck <joseph.luck@sky.com>
2021-02-01 18:05:39 +0000

    Free up shift keys as tapping / hold behaviour for my typing style is no good...

--- keyboards/keebio/iris/keymaps/josephluck/keymap.c
+++ keyboards/keebio/iris/keymaps/josephluck/keymap.c
@@ -20,7 +20,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
      KC_MINS,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                               KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
   //├────────┼────────┼────────┼────────┼────────┼────────┼────────┐        ┌────────┼────────┼────────┼────────┼────────┼────────┼────────┤
- SFT_T(KC_GRV),LCTL_T(KC_Z),KC_X,KC_C,    KC_V,    KC_B,  LT(_UTILS,KC_DEL),  KC_NO,   KC_N,    KC_M,   KC_COMM,  KC_DOT, RCTL_T(KC_SLSH), RSFT_T(KC_ESC),
+     KC_LSFT,LCTL_T(KC_Z),KC_X,  KC_C,    KC_V,    KC_B,  LT(_UTILS,KC_DEL),  KC_NO,   KC_N,    KC_M,   KC_COMM,  KC_DOT, RCTL_T(KC_SLSH), KC_RSFT,
   //└────────┴────────┴────────┴───┬────┴───┬────┴───┬────┴───┬────┘        └───┬────┴───┬────┴───┬────┴───┬────┴────────┴────────┴────────┘
                             LALT_T(KC_UP), LGUI_T(KC_DOWN), LT(_RAISE, KC_SPC),    LT(_RAISE,KC_ENT), RGUI_T(KC_LEFT), RALT_T(KC_RGHT)
                                 // └────────┴────────┴────────┘                 └────────┴────────┴────────┘
@@ -28,13 +28,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   [_RAISE] = LAYOUT(
   //┌────────┬────────┬────────┬────────┬────────┬────────┐                          ┌────────┬────────┬────────┬────────┬────────┬────────┐
-      KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,                              KC_NO,   KC_7,    KC_8,    KC_9,    KC_NO,   KC_NO,
+      KC_ESC,  KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,                              KC_NO,   KC_7,    KC_8,    KC_9,    KC_NO,   KC_DEL,
   //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
       KC_NO,   KC_NO,  KC_LPRN, KC_LBRC, KC_LCBR,  KC_NO,                              KC_NO,   KC_4,    KC_5,    KC_6,    KC_NO,   KC_NO,
   //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
-      KC_NO,   KC_NO,  KC_RPRN, KC_RBRC, KC_RCBR,  KC_NO,                              KC_NO,   KC_1,    KC_2,    KC_3,    KC_NO,   KC_NO,
+      KC_NO,   KC_NO,  KC_RPRN, KC_RBRC, KC_RCBR,  KC_NO,                              KC_NO,   KC_1,    KC_2,    KC_3,    KC_NO,   KC_GRV,
   //├────────┼────────┼────────┼────────┼────────┼────────┼────────┐        ┌────────┼────────┼────────┼────────┼────────┼────────┼────────┤
-      KC_NO,   KC_NO,  KC_LT,   KC_SLSH,  KC_GT,   KC_NO,   KC_NO,            KC_NO,   KC_NO,   KC_0,    KC_DOT,  KC_SLSH, KC_NO,   KC_NO,
+      KC_TILDE,KC_NO,  KC_LT,   KC_SLSH,  KC_GT,   KC_NO,   KC_NO,            KC_NO,   KC_NO,   KC_0,    KC_DOT,  KC_SLSH, KC_NO,   KC_TILDE,
   //└────────┴────────┴────────┴───┬────┴───┬────┴───┬────┴───┬────┘        └───┬────┴───┬────┴───┬────┴───┬────┴────────┴────────┴────────┘
                                      KC_NO,   KC_NO,   KC_DEL,                    KC_NO,   KC_NO,   KC_NO                                
                                 // └────────┴────────┴────────┘                 └────────┴────────┴────────┘
