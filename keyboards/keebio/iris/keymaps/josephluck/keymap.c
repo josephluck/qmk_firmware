5f80da1ea46525a412c7659b632f42ad0d3bacb7
Joseph Luck <joseph.luck@sky.com>
2021-02-23 18:06:03 +0000

    Iris corne layout

--- keyboards/keebio/iris/keymaps/josephluck/keymap.c
+++ keyboards/keebio/iris/keymaps/josephluck/keymap.c
@@ -14,29 +14,29 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   [_QWERTY] = LAYOUT(
   //┌────────┬────────┬────────┬────────┬────────┬────────┐                          ┌────────┬────────┬────────┬────────┬────────┬────────┐
-     KC_TAB,   KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                               KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC,
+     KC_NO ,   KC_1   , KC_2   , KC_3   , KC_4   , KC_5,                               KC_6   , KC_7   , KC_8   , KC_9   , KC_0   , KC_NO  ,
   //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
-     KC_EQL,   KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                               KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSLS,
+     KC_ESC,   KC_Q   , KC_W   , KC_E   , KC_R   , KC_T,                               KC_Y   , KC_U   , KC_I   , KC_O   , KC_P   , KC_BSLS,
   //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
-     KC_MINS,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                               KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
+     KC_EQL  , KC_A   , KC_S   , KC_D   , KC_F   , KC_G,                               KC_H   , KC_J   , KC_K   , KC_L   , KC_SCLN, KC_QUOT,
   //├────────┼────────┼────────┼────────┼────────┼────────┼────────┐        ┌────────┼────────┼────────┼────────┼────────┼────────┼────────┤
-     KC_LSFT,LCTL_T(KC_Z),KC_X,  KC_C,    KC_V,    KC_B,  LT(_UTILS,KC_DEL),  KC_NO,   KC_N,    KC_M,   KC_COMM,  KC_DOT, RCTL_T(KC_SLSH), KC_RSFT,
+     KC_LSFT,LCTL_T(KC_Z),KC_X , KC_C   , KC_V   , KC_B   ,LT(_UTILS,KC_DEL),  KC_NO,  KC_N   , KC_M   , KC_COMM, KC_DOT , RCTL_T(KC_SLSH), KC_RSFT,
   //└────────┴────────┴────────┴───┬────┴───┬────┴───┬────┴───┬────┘        └───┬────┴───┬────┴───┬────┴───┬────┴────────┴────────┴────────┘
-                            LALT_T(KC_UP), LGUI_T(KC_DOWN), LT(_RAISE, KC_SPC),    LT(_RAISE,KC_ENT), RGUI_T(KC_LEFT), RALT_T(KC_RGHT)
+                            LALT_T(KC_UP), LGUI_T(KC_DOWN), LT(_RAISE, KC_SPC),    LT(_RAISE,KC_BSPC), RGUI_T(KC_LEFT), RALT_T(KC_RGHT)
                                 // └────────┴────────┴────────┘                 └────────┴────────┴────────┘
   ),
 
   [_RAISE] = LAYOUT(
   //┌────────┬────────┬────────┬────────┬────────┬────────┐                          ┌────────┬────────┬────────┬────────┬────────┬────────┐
-      KC_ESC,  KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,                              RESET,   KC_7,    KC_8,    KC_9,    KC_NO,   KC_DEL,
+      KC_NO  , KC_NO  , KC_NO  , KC_NO  , KC_NO  , KC_NO  ,                            KC_NO  , KC_NO  , KC_NO  , KC_NO  , KC_NO  , KC_DEL ,
   //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
-      KC_NO,   KC_NO,  KC_LPRN, KC_LBRC, KC_LCBR,  KC_NO,                              KC_NO,   KC_4,    KC_5,    KC_6,    KC_NO,   KC_NO,
+      KC_MINS, KC_EXLM, KC_AT  ,KC_HASH , KC_DLR , KC_PERC,                            RESET  , KC_7   , KC_8   , KC_9   , KC_0   , KC_PLUS,
   //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
-      KC_NO,   KC_NO,  KC_RPRN, KC_RBRC, KC_RCBR,  KC_NO,                              KC_NO,   KC_1,    KC_2,    KC_3,    KC_NO,   KC_GRV,
+      KC_UNDS, KC_AMPR, KC_LPRN,KC_LBRC ,KC_LCBR , KC_ASTR,                            KC_HASH, KC_4   , KC_5   , KC_6   , KC_NO  , KC_GRV ,
   //├────────┼────────┼────────┼────────┼────────┼────────┼────────┐        ┌────────┼────────┼────────┼────────┼────────┼────────┼────────┤
-      KC_TILDE,KC_NO,  KC_LT,   KC_SLSH,  KC_GT,   KC_NO,   KC_NO,            KC_NO,   KC_NO,   KC_0,    KC_DOT,  KC_SLSH, KC_NO,   KC_TILDE,
+      KC_TILDE,KC_CIRC, KC_RPRN,KC_RBRC ,KC_RCBR , KC_NO  , KC_NO  ,          KC_NO  , KC_NO  , KC_1   , KC_2   , KC_3   , KC_BSLS, KC_PIPE,
   //└────────┴────────┴────────┴───┬────┴───┬────┴───┬────┴───┬────┘        └───┬────┴───┬────┴───┬────┴───┬────┴────────┴────────┴────────┘
-                            LALT_T(KC_UP), LGUI_T(KC_DOWN), KC_DEL,              LT(_RAISE,KC_ENT), RGUI_T(KC_LEFT), RALT_T(KC_RGHT)                               
+                            LALT_T(KC_UP), LGUI_T(KC_DOWN), KC_TAB,              KC_ENT, RGUI_T(KC_LEFT), RALT_T(KC_RGHT)                               
                                 // └────────┴────────┴────────┘                 └────────┴────────┴────────┘
   ),
 
