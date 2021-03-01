b85214dd44f4e7a43a722c0928b43a04267d1632
Joseph Luck <joseph.luck@sky.com>
2021-03-01 14:44:45 +0000

    Create corne keymap

---
+++ keyboards/crkbd/keymaps/josephluck/rules.mk
@@ -0,0 +1,8 @@
+# Build Options
+#   change to "no" to disable the options, or define them in the Makefile in
+#   the appropriate keymap folder that will get included automatically
+#
+RGBLIGHT_ENABLE         = no
+RGB_MATRIX_ENABLE       = no
+
+OLED_DRIVER_ENABLE      = yes
